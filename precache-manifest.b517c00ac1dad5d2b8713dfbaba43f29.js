3c85e1bc4a5656aa739c52949eb04b03272d61f4
Shalika <shalikaashan01@gmail.com>
2019-09-02 19:28:09 +0530

    Updates

---
+++ precache-manifest.b517c00ac1dad5d2b8713dfbaba43f29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d43ecc41441bf74029efaaaee9393d39",
+    "url": "/pwa-sample/index.html"
+  },
+  {
+    "revision": "b5e9c9b08046e53e5624",
+    "url": "/pwa-sample/static/css/2.ad9eb4b5.chunk.css"
+  },
+  {
+    "revision": "d214694f92aa78e66197",
+    "url": "/pwa-sample/static/css/main.b9c11dbb.chunk.css"
+  },
+  {
+    "revision": "b5e9c9b08046e53e5624",
+    "url": "/pwa-sample/static/js/2.17e9c0cd.chunk.js"
+  },
+  {
+    "revision": "d214694f92aa78e66197",
+    "url": "/pwa-sample/static/js/main.d0ef8f05.chunk.js"
+  },
+  {
+    "revision": "58dcb99525050ebf0920",
+    "url": "/pwa-sample/static/js/runtime~main.c3ba0da5.js"
+  }
+]);
